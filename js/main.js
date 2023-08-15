8363d2a23f8abd9b0f47a0d81e4ae87b269a555c
Artem Dori <98597370+ABDori@users.noreply.github.com>
2023-08-15 04:29:12 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+//burger
+
 document.querySelector('.burger').addEventListener('click',function (e) {
 
       document.querySelector('.main_nav').classList.toggle('hidden');
       document.querySelector('.burger').classList.toggle('active');
       document.querySelector('body').classList.toggle('fixed');
       e.preventDefault();
+});
+
+
+//intersection observer
+
+const observedSec = document.querySelectorAll('.section_container');
+const options = {
+      root: null,
+      threshold: 0.2,
+      rootMargin: "-100px"
+};
+
+const observer = new IntersectionObserver(function (entries, observer){
+      entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                  entry.target.classList.remove('moved');
+              }
+})}, options);
+ 
+
+observedSec.forEach(observedSec => {
+      observer.observe(observedSec);
 });
